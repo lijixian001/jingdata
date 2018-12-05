2ece1f0cc551b0e7a1d097b2a34074e118db9a5e
lijixian <lijixian@a7hr.com>
2018-12-05 16:22:52 +0800

    V20181205003 Cookie过期修改

--- config/url.py
+++ config/url.py
@@ -2,7 +2,7 @@
 #alpha
 
 headers = {'Cookie': 'acw_tc=3ccdc15215435988465487129e53b8fc7adc273e227d84dfc69f330ce72595; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2216765a934bcc-023801d6dbed72-8383268-1327104-16765a934bd20b%22%2C%22%24device_id%22%3A%2216765a934bcc-023801d6dbed72-8383268-1327104-16765a934bd20b%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D; dfp=CirvoHIC3wUNPhzjneMzi5pyHo36SWhGeXc7sJq6KOKmxCT1MM4x+QzSsy+9a3KQ2iYTvVzb3b7214yqhqf4T0n/uXvGDOAi0f+eHNtvAAsSEDOGmQPC5v83Op8zanH2BvwqTCYuzPf156ZzdaRwbPevmdN4z8Q8oUhxHPlGE5w%3D; insight_uid=794; laravel_session=xknzSoqZMN3f7SQ8MPGNEeLkmEVuwAN3H0v9DhN7'}
-#生产环境
+#生产环境Cookie
 
 #url = "http://insight-alpha.jingdata.com"
 url = "https://insight.jingdata.com"
